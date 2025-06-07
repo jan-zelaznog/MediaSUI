d0685d5383853e22be5918fb6690552c90b1a8a6
jan-zelaznog <jan.zelaznog@gmail.com>
2025-06-07 13:30:49 -0600

    Initial Commit

---
+++ MediaSUI/MediaSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MediaSUIApp.swift
+//  MediaSUI
+//
+//  Created by Ángel González on 07/06/25.
+//
+
+import SwiftUI
+
+@main
+struct MediaSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
